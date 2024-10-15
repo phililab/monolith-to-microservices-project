3869da13d390982a9fb83fc3ef28ed7927b33f3f
Philippe Labonde <philippe.labonde@arvato-scs.com>
2024-10-15 08:39:40 +0200

    refactor k8s and fix k8s routing

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a41cfa83ba7184f22b20f274f65d9499-919685270.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
